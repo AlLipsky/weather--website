cd4a914b1d07bb8b23745857a65af55c1f0d0b68
Alexander Lipsky <senderlipsky@gmail.com>
2020-05-26 11:28:13 +0300

    package

--- weathe-app/app.js
+++ weathe-app/app.js
@@ -1,22 +1,22 @@
-const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast')
-const userGeoPlace = process.argv[2];
+// const geocode = require('./utils/geocode');
+// const forecast = require('./utils/forecast')
+// const userGeoPlace = process.argv[2];
 
-if (!userGeoPlace) {
-    return console.log('Please enter location');
-}
-else {
-    geocode(userGeoPlace, (error, {longtitude, latitude, location} = {}) => {
-        if (error) {
-            return console.log(error);
-        }
-        forecast(longtitude, latitude, (error, forecastData) => {
-            console.log(location)
-            console.log(forecastData);
-        })
-    })
+// if (!userGeoPlace) {
+//     return console.log('Please enter location');
+// }
+// else {
+//     geocode(userGeoPlace, (error, {longtitude, latitude, location} = {}) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         forecast(longtitude, latitude, (error, forecastData) => {
+//             console.log(location)
+//             console.log(forecastData);
+//         })
+//     })
 
-    console.log('argv', process.argv);
-    console.log('argv', process.argv[2]);
+//     console.log('argv', process.argv);
+//     console.log('argv', process.argv[2]);
 
-}
+// }
